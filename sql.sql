0eea24d813f612b8baf6673229dc65bd955bff44
Kez <79928650+KezCoding@users.noreply.github.com>
2021-07-08 04:21:10 +0200

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `cringefr_mth`
+-- Base de données : `by kzxed bro`
 --
 
 -- --------------------------------------------------------
